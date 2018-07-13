e74f8c26f3e814430ca5af6fecd44a7618718b9f
RTshok <fireart29@gmail.com>
2018-07-13 12:53:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <string.h>
 #include "list.h"
 #include "Sort_list.h"
-
+/**
+* That is the implementation of my linked list, you could use any data type(but then you need to add cmp function for sorting, and improve printing function to print)
+* Each list can handle only one type of data.
+* 
+*/
 int main () {
 
 TestIntList();
